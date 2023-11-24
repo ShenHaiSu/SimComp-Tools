15b8d1b04c5b4491d2902572729dbf19d8dbfe45
DaoluoLTS_Kim <2216851340@qq.com>
2023-11-24 16:21:55 +0800

    2023-11-24 16:15:48
    - `零售显示总利润/时利润/建议定价` 脱离API数据,不再使用计算式,使用模拟用户键入形式
    - `零售显示总利润/时利润/建议定价` 添加按钮允许使用固定时利润定义价格

--- components/retailDisplayProfit.js
+++ components/retailDisplayProfit.js
@@ -13,30 +13,15 @@ class retailDisplayProfit extends BaseComponent {
   commonFuncList = [{
     match: () => Boolean(location.href.match(/\/b\/\d+\//)) && document.activeElement.name == "price" && document.activeElement.tagName == "INPUT",
     func: this.mainFunc
-  }];
+  }]
   componentData = {
     fadeTimer: undefined, // 自动消失计时器标签
     containerNode: undefined, // 显示容器元素
-    lastCountTimeStamp: 0, // 最近一次计算的时间戳
-    adminRate: 0, // 管理费比率
-    retaiInfoList: [], // 零售物品信息库 [{id,name,price,marketSaturation.....}]
-    recommendList: [], // 推荐定价列表 [id:121]
-    apiDataList: {}, // API的数据列表 {G:{resources:{}}}
-    lastNetTime: 0, // 最近一次请求网络
-    lockProfit: {  // 锁定时利润
-      isLock: false,
-      profit: 0.0
-    },
-    lastActiveInputNode:undefined, // 最后一次激活中的input标签
+    lastActiveInputNode: undefined, // 最后一次激活中的input标签
   }
-  netFuncList = [{
-    urlMatch: (url) => Boolean(url.match(/administration-overhead\/$/)),
-    func: this.adminRateGet
-  }, {
-    urlMatch: (url) => Boolean(url.match(/encyclopedia\/resources\/\d+\/\d+\/$/)),
-    func: this.getResourcesRetailInfo
-  }]
-  cssText = [`#retail_display_div {color:var(--fontColor);padding:5px;border-radius:5px;background-color:rgba(0, 0, 0, 0.5);position:fixed;top:50%;right:0;transform: translateY(-50%);width:150px;z-index:1032;justify-content:center;align-items:center;}`];
+  cssText = [
+    `#retail_display_div {color:var(--fontColor);padding:5px;border-radius:5px;background-color:rgba(0, 0, 0, 0.5);position:fixed;top:50%;right:0;transform: translateY(-50%);width:160px;z-index:1032;justify-content:center;align-items:center;}#retail_display_div button{background:#6B6B6B;margin-top:5px;}`
+  ];
 
   async mainFunc() {
     // 初始化
@@ -44,8 +29,8 @@ class retailDisplayProfit extends BaseComponent {
     let activeNodeRect = activeNode.getBoundingClientRect();
     let targetNode = tools.getParentByIndex(activeNode, 5).previousElementSibling.querySelector("div > div > h3").parentElement;
     let quantity = tools.getParentByIndex(activeNode, 2).previousElementSibling.querySelector("div > p > input[name='quantity']").value;
-    let quality = this.getQuality(activeNode);
-    let buildingLevel = parseInt(Object.values(document.querySelectorAll("div>span>b")).filter(node => /\d+级/.test(node.innerText))[0].innerText);
+    // let quality = this.getQuality(activeNode);
+    // let buildingLevel = parseInt(Object.values(document.querySelectorAll("div>span>b")).filter(node => /\d+级/.test(node.innerText))[0].innerText);
     let price = activeNode.value;
     let baseInfo;
     try { baseInfo = this.getInfo(targetNode) } catch (error) { return }
@@ -69,27 +54,22 @@ class retailDisplayProfit extends BaseComponent {
       this.componentData.containerNode = newNode;
       document.body.appendChild(newNode);
       // 挂载锁定时利润事件委派
-      newNode.addEventListener('click', event => this.lockProfitHandle(event));
+      newNode.addEventListener('click', event => this.clickEventHandle(event));
     }
 
-    // 计算推荐价格
-    let [recommendPrice, maxHourProfit] = await this.genRecommendPrice(tools.itemName2Index(baseInfo.name), quality, quantity, price, buildingLevel);
-
     // 填充内容
-    let totalProfit = (baseInfo.profit * quantity).toFixed(3);
-    let hourProfit = (totalProfit / baseInfo.duration_hour).toFixed(3);
-    // console.log("hourProfit", parseFloat(hourProfit), "maxHourProfit", maxHourProfit, "差值", parseFloat(hourProfit) - maxHourProfit);
-    // let htmlText = `预估数据：\n总利润：${totalProfit}\n时利润：${hourProfit} <input type='checkbox' ${this.componentData.lockProfit.isLock ? "checked" : ""}>锁定</input>\n参考数据:${recommendPrice}`
+    let totalProfit = parseFloat((baseInfo.profit * quantity).toFixed(2));
+    let hourProfit = parseFloat((totalProfit / baseInfo.duration_hour).toFixed(2));
     let htmlText = `<div>预估数据: </div>`;
     htmlText += `<div>总利润：${totalProfit}</div>`;
-    // htmlText += `<div>时利润：${hourProfit} <input id='script_lockProfit' type='checkbox' ${this.componentData.lockProfit.isLock ? "checked" : ""}>锁定</div>`;
     htmlText += `<div>时利润：${hourProfit}</div>`;
-    htmlText += `<div>参考数据：${recommendPrice}</div>`;
+    htmlText += `<button class='btn' id='script_reatil_maxHour'>max时利润</button>`;
+    htmlText += `<button class='btn' id='script_reatil_targetHour'>锁定时利润</button>`;
     this.componentData.containerNode.innerHTML = htmlText;
     Object.assign(this.componentData.containerNode.style, {
       display: "block",
-      top: `${activeNodeRect.top + activeNodeRect.height + 64}px`,
-      left: `${activeNodeRect.left}px`,
+      top: `${activeNodeRect.top + activeNodeRect.height + 80}px`,
+      left: `${activeNodeRect.left - 20}px`,
     })
 
     // 创建计时器
@@ -110,12 +90,11 @@ class retailDisplayProfit extends BaseComponent {
     let [targetHour, targetMinutes] = targetStamp.split(":");
     let targetTime = new Date(nowTime.getFullYear(), nowTime.getMonth(), nowTime.getDate(), targetHour, targetMinutes);
     let timeDiff = parseFloat(((targetTime.getTime() - nowTime.getTime()) / (1000 * 60 * 60)).toFixed(3));
-    let dayCount = 0;
-    // 获取日数
-    dayCount += (/(\d+)d/.test(durationTime)) ? parseInt(durationTime.match(/(\d+)d/)[1]) : 0;
-    // 获取周数
-    dayCount += (/(\d+)w/.test(durationTime)) ? parseInt(durationTime.match(/(\d+)w/)[1]) * 7 : 0;
-    timeDiff += (timeDiff < 0) ? ((dayCount + 1) * 24) : dayCount * 24;
+    let exactOffect = 0;
+    // 获取分钟与秒
+    exactOffect += (/(\d+)d/.test(durationTime)) ? parseInt(durationTime.match(/(\d+)d/)[1]) : 0;
+    exactOffect += (/(\d+)w/.test(durationTime)) ? parseInt(durationTime.match(/(\d+)w/)[1]) * 7 : 0;
+    timeDiff += (timeDiff < 0) ? ((exactOffect + 1) * 24) : exactOffect * 24;
     tools.log(`销售完成时间:${new Date(new Date().getTime() + timeDiff * 60 * 60 * 1000).toLocaleString()}`);
     return timeDiff;
   }
@@ -150,94 +129,75 @@ class retailDisplayProfit extends BaseComponent {
     }
     return (totalCost / nowQuantity).toFixed(2);
   }
-  // 锁定当前时利润事件委派
-  lockProfitHandle(event) {
-    if (event.target.tagName != "INPUT" || event.target.id != "script_lockProfit") return;
-    this.componentData.lockProfit.profit = parseFloat(event.target.parentElement.innerText.replace(/时利润：|锁定/g, ""));
-    this.componentData.lockProfit.isLock = event.target.checked;
-    console.log(this.componentData.lockProfit);
-    // 清空原有计算记录
-    this.componentData.lastCountTimeStamp = 0;
-    this.componentData.lastNetTime = 0;
+  // 按钮时间委派
+  clickEventHandle(event) {
+    // if (event.target.tagName == "INPUT" && event.target.id == "script_lockProfit") return this.lockHourProfit(event);
+    if (event.target.tagName == "BUTTON" && event.target.id == "script_reatil_maxHour") return this.setMaxProfitPrice(event);
+    if (event.target.tagName == "BUTTON" && event.target.id == "script_reatil_targetHour") return this.lockHourProfit(event);
   }
-  // 拦截保存账号管理费比率
-  adminRateGet(url, method, resp) {
-    let realm = runtimeData.basisCPT.realm;
-    let adminRate = parseFloat(resp) - 1;
-    indexDBData.basisCPT.userInfo[realm].adminRate = adminRate;
-    this.componentData.adminRate = adminRate;
-  }
-  // 拦截物品的零售数据
-  getResourcesRetailInfo(url, method, resp) {
-    let newResp = JSON.parse(resp);
-    let resID = parseInt(url.match(/\d+\/$/)[0].replace("/", ""));
-    this.componentData.retaiInfoList[resID] = newResp;
-  }
-  // 生成推荐定价的提前检测与获取
-  async genRecommendPrice(resID = 0, quality = 0, quantity = 0, nowPrice = 0, buildingLevel) {
-    let timeStamp = new Date().getTime();
-    if (timeStamp - this.componentData.lastCountTimeStamp < 2000) return [this.lastPrice(resID), 0];
-    let realm = runtimeData.basisCPT.realm;
-    let buildID = parseInt(location.href.match(/\d+\/$/)[0].replace("/", ""));
-    let buildKind = tools.getBuildKind(buildID);
-    let fileName = `${buildKind}-${realm == 0 ? "R1" : "R2"}.json`;
-    // 获取API数据
-    let apiData = this.componentData.apiDataList[buildKind] || await this.innerGetNetData(fileName);
-    if (!apiData) return [0.0, 0];
-    this.componentData.apiDataList[buildKind] = apiData;
-    let saturation = this.componentData.retaiInfoList[resID].marketSaturation;
-    let retail_modeling = apiData.resources[resID.toString()].retail_modeling;
-    let sellBonus = indexDBData.basisCPT.userInfo[realm].authCompany.salesModifier;
-    let building_wages = Object.values(apiData.resources)[0].building_wages;
-    let adminRate = this.componentData.adminRate;
-    let cost = this.getCost(tools.itemIndex2Name(resID), quantity);
-    let maxSellPrice = nowPrice * 0.5;
-    let maxHourProfit = 0;
-    for (let index = maxSellPrice; index < nowPrice * 1.5; index += 0.01) {
-      let newHourProfit = this.countOutHourProfit(index, saturation, retail_modeling, quality, sellBonus, building_wages, adminRate, cost);
-      newHourProfit *= buildingLevel;
-      if (this.componentData.lockProfit.isLock) {
-        if (newHourProfit < this.componentData.lockProfit.profit) continue;
-        // console.log(index, newHourProfit);
-        maxSellPrice = index;
-        maxHourProfit = newHourProfit;
+  // 锁定当前时利润并求最小价格
+  lockHourProfit(event) {
+    let targetHourProfit = window.prompt("输入期望的小时收益", "0.0");
+    if (isNaN(parseFloat(targetHourProfit))) return;
+    try {
+      // 锁定填写框
+      this.componentData.lastActiveInputNode.disabled = true;
+      // 获取数据
+      let targetNode = tools.getParentByIndex(this.componentData.lastActiveInputNode, 5).previousElementSibling.querySelector("div > div > h3").parentElement;
+      let quantity = tools.getParentByIndex(this.componentData.lastActiveInputNode, 2).previousElementSibling.querySelector("div > p > input[name='quantity']").value;
+      let basePrice = parseFloat(this.componentData.lastActiveInputNode.value) * 0.5;
+      let maxPrice = basePrice * 3;
+      let maxProfit = parseFloat(targetHourProfit);
+      let step = this.getStep(maxPrice);
+      let baseInfo;
+      for (let tampPrice = basePrice; tampPrice < maxPrice; tampPrice += step) {
+        tools.setInput(this.componentData.lastActiveInputNode, tampPrice);
+        baseInfo = this.getInfo(targetNode);
+        let tempProfit = parseFloat(baseInfo.profit * quantity / baseInfo.duration_hour);
+        if (tempProfit <= maxProfit) continue;
+        basePrice = tampPrice;
         break;
-      } else {
-        if (newHourProfit - maxHourProfit <= 0) continue;
-        maxSellPrice = index;
-        maxHourProfit = newHourProfit;
       }
+      tools.log("价格", basePrice, "时利润", maxProfit);
+      tools.setInput(this.componentData.lastActiveInputNode, basePrice);
+    } finally {
+      // 解锁填写框
+      this.componentData.lastActiveInputNode.disabled = false;
     }
-
-    // 价格格式化
-    if (maxSellPrice <= 8) maxSellPrice = maxSellPrice.toFixed(2);
-    if (maxSellPrice > 8 && maxSellPrice <= 500) maxSellPrice = maxSellPrice.toFixed(1);
-    if (maxSellPrice > 500) maxSellPrice = maxSellPrice.toFixed(0);
-
-    this.componentData.recommendList[resID] = maxSellPrice;
-    this.componentData.lastCountTimeStamp = timeStamp;
-    return [maxSellPrice, maxHourProfit];
   }
-  // 计算时利润
-  countOutHourProfit(sellprice, saturation, retail_modeling, quality, sellBonus, building_wages, adminRate, courcCost) {
-    let time2sellPerUnit = Math.pow(sellprice * retail_modeling.xMultiplier + (retail_modeling.xOffsetBase + (Math.max(-.38, saturation - .24 * quality) - .5) / retail_modeling.marketSaturationDiv), retail_modeling.power) * retail_modeling.yMultiplier + retail_modeling.yOffset;
-    time2sellPerUnit = time2sellPerUnit.toFixed(2);
-    let unitsSoldPerHour = 3600 / time2sellPerUnit / (1 - sellBonus / 100);
-    unitsSoldPerHour = unitsSoldPerHour.toFixed(2);
-    let revenuesPerHour = unitsSoldPerHour * sellprice;
-    revenuesPerHour = revenuesPerHour.toFixed(2);
-    let cost = courcCost * unitsSoldPerHour + building_wages + (building_wages * adminRate / 100);
-    return revenuesPerHour - cost;
+  // 将价格设定为最大时利润
+  setMaxProfitPrice(event) {
+    try {
+      // 锁定填写框
+      this.componentData.lastActiveInputNode.disabled = true;
+      // 获取数据
+      let targetNode = tools.getParentByIndex(this.componentData.lastActiveInputNode, 5).previousElementSibling.querySelector("div > div > h3").parentElement;
+      let quantity = tools.getParentByIndex(this.componentData.lastActiveInputNode, 2).previousElementSibling.querySelector("div > p > input[name='quantity']").value;
+      let basePrice = parseFloat(this.componentData.lastActiveInputNode.value) * 0.5;
+      let maxPrice = basePrice * 3;
+      let maxProfit = 0.0;
+      let step = this.getStep(maxPrice);
+      let baseInfo;
+      for (let tampPrice = basePrice; tampPrice < maxPrice; tampPrice += step) {
+        tools.setInput(this.componentData.lastActiveInputNode, tampPrice);
+        baseInfo = this.getInfo(targetNode);
+        let tempProfit = parseFloat(baseInfo.profit * quantity / baseInfo.duration_hour);
+        if (tempProfit <= maxProfit) continue;
+        maxProfit = tempProfit;
+        basePrice = tampPrice;
+      }
+      tools.log("价格", basePrice, "时利润", maxProfit);
+      tools.setInput(this.componentData.lastActiveInputNode, basePrice);
+    } finally {
+      // 解锁填写框
+      this.componentData.lastActiveInputNode.disabled = false;
+    }
   }
-  // 获取上一次计算的结果 或者 0.0
-  lastPrice(resID) {
-    return this.componentData.recommendList[resID] || 0.0;
-  }
-  async innerGetNetData(fileName) {
-    let nowTime = new Date().getTime();
-    if (nowTime - this.componentData.lastNetTime <= 10000) return false;
-    this.componentData.lastNetTime = nowTime;
-    return tools.getNetData(`https://cdn.jsdelivr.net/gh/ShenHaiSu/SimComp-APIProxy@main/toolsData/least/${fileName}?${await tools.generateUUID()}`);
+  // 获取步长
+  getStep(basePrice) {
+    if (basePrice <= 8) return 0.01;
+    if (basePrice <= 500) return 0.1;
+    return 1;
   }
 }
 new retailDisplayProfit();
