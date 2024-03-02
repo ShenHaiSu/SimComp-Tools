af3a0f55076c0fcfd37d0d1f884af5197ca5882e
DaoluoLTS_Kim <2216851340@qq.com>
2024-03-02 20:21:16 +0800

    2024-3-2 20:18:18
    - `空闲建筑高亮`
      - 针对销售大楼做了额外处理
      - 在地图界面会逐个大楼获取订单信息。根据结果逐个添加高亮。请求间隔默认10s
      - 现在销售大楼只会在当前无订单并且未在询单时符合高亮规则

--- components/unBusyHighLight.js
+++ components/unBusyHighLight.js
@@ -10,7 +10,7 @@ class unBusyHighLight extends BaseComponent {
     this.tagList = ['样式', "建筑"];
   }
   componentData = {
-    blackList: ["n"], // 屏蔽的建筑类型 n 银行 
+    blackList: ["n", "B"], // 屏蔽的建筑类型 n 银行
   }
   commonFuncList = [{
     match: () => Boolean(/landscape\/$/.test(location.href)),
@@ -21,17 +21,42 @@ class unBusyHighLight extends BaseComponent {
   ]
 
   async mainFunc() {
+    if (Object.values(document.querySelectorAll("div#page>div>div>div>div>a")).length === 0) return;
+    this.clearClassName();
     let realm = await tools.getRealm();
+
+    // 生成当前不忙的建筑数组 跳过大楼建筑
     let unBusyList = indexDBData.basisCPT.building[realm]
       .filter(build => build.busy == undefined && !this.componentData.blackList.includes(build.kind))
       .map(build => build.id + "");
+    this.changeClassName(unBusyList);
+
+    // 额外处理大楼建筑
+    let unBustSaleOfficeList = indexDBData.basisCPT.building[realm].filter(build => build.kind == "B" && !Boolean(build.salesContract));
+    for (let i = 0; i < unBustSaleOfficeList.length; i++) {
+      let officeContrate = await tools.getNetData(`https://www.simcompanies.com/api/v2/companies/buildings/${unBustSaleOfficeList[i].id}/sales-orders/`);
+      // console.log(`${unBustSaleOfficeList[i].id} 建筑已有单：`, officeContrate);
+      if (!officeContrate) continue;
+      if (officeContrate.length == 0) this.changeClassName([unBustSaleOfficeList[i].id + ""])
+    }
+  }
+
+  // 捕获建筑dom标签并过滤
+  changeClassName(unBusyList) {
     let buildingList = Object.values(document.querySelectorAll("div#page>div>div>div>div>a"))
       .filter(build => /\/b\/(\d+)\/$/.test(build.href) && unBusyList.includes(build.href.match(/\/b\/(\d+)\/$/)[1]))
       .filter(build => !/script_unBusyHighLight_aTag/.test(build.className));
 
     for (let i = 0; i < buildingList.length; i++) {
-      ``
       buildingList[i].className += ` script_unBusyHighLight_aTag`;
+    }
+  }
+
+  // 清除所有的tag
+  clearClassName() {
+    let buildingList = Object.values(document.querySelectorAll("div#page>div>div>div>div>a"));
+    for (let i = 0; i < buildingList.length; i++) {
+      buildingList[i].className.replace("script_unBusyHighLight_aTag", "");
     }
   }
 }
