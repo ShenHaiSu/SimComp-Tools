67da592734230b52089aac48921424b73943300d
DaoLuoLTS <61882306+ShenHaiSu@users.noreply.github.com>
2025-07-19 23:44:57 +0800

    refactor(biggerAmcharts): 重构图表放大组件并优化代码结构
    将类名改为大驼峰命名规范，拆分主函数为两个独立方法处理不同页面
    使用文档片段减少DOM操作，优化性能
    添加代码注释提高可读性
    
    docs(bondTracker): 为债券追踪器添加详细注释
    添加类和方法注释说明功能
    优化设置表单的DOM查询性能
    使用模板字符串生成债券信息

--- components/biggerAmcharts.js
+++ components/biggerAmcharts.js
@@ -1,8 +1,8 @@
 const BaseComponent = require("../tools/baseComponent.js");
-const { tools, componentList, runtimeData, indexDBData, feature_config } = require("../tools/tools.js");
+const { tools } = require("../tools/tools.js");
 
 // 总览与财报界面图表放大
-class biggerAmcharts extends BaseComponent {
+class BiggerAmcharts extends BaseComponent {
   constructor() {
     super();
     this.name = "图表放大";
@@ -10,29 +10,50 @@ class biggerAmcharts extends BaseComponent {
     this.enable = true;
     this.tagList = ["样式", "图表"];
   }
+
+  // 定义匹配规则和对应处理函数
   commonFuncList = [{
     match: () => Boolean(location.href.match(/headquarters\/(accounting\/|overview\/$)/)),
     func: this.mainFunc
   }];
+
+  // 处理图表放大的主函数
   mainFunc() {
-    let url = location.href;
+    const CHART_HEIGHT = "600px";
+    const url = location.href;
+
     if (url.endsWith("headquarters/overview/")) {
-      // 总览界面
-      let target_node = document.querySelector("div.row > div.col-sm-6 > div > div > div");
-      Object.assign(target_node.style, { height: "600px" });
-      let msg_node = tools.getParentByIndex(target_node, 4).lastChild;
-      msg_node.className = "col-sm-6 text-center";
-      if (msg_node.querySelectorAll("br").length > 1) return;
-      msg_node.lastChild.prepend(document.createElement("br"));
-      msg_node.lastChild.prepend(document.createElement("br"));
-      msg_node.lastChild.prepend(document.createElement("br"));
+      this.handleOverviewPage(CHART_HEIGHT);
     } else {
-      // 财务界面
-      let target_node = document.querySelector("div.col-md-9 > div > div > div > div > div.amcharts-main-div");
-      target_node = target_node.parentElement;
-      Object.assign(target_node.style, { height: "600px" });
-      // target_node.style.height = "600px";
+      this.handleAccountingPage(CHART_HEIGHT);
     }
   }
+
+  // 处理总览页面的图表放大
+  handleOverviewPage(height) {
+    const chartNode = document.querySelector("div.row > div.col-sm-6 > div > div > div");
+    Object.assign(chartNode.style, { height });
+
+    const messageNode = tools.getParentByIndex(chartNode, 4).lastChild;
+    messageNode.className = "col-sm-6 text-center";
+
+    // 避免重复添加换行
+    if (messageNode.querySelectorAll("br").length > 1) return;
+
+    // 使用文档片段一次性添加三个换行符，减少DOM操作
+    const fragment = document.createDocumentFragment();
+    Array(3).fill().forEach(() => {
+      fragment.appendChild(document.createElement("br"));
+    });
+    messageNode.lastChild.prepend(fragment);
+  }
+
+  // 处理财务页面的图表放大
+  handleAccountingPage(height) {
+    const chartNode = document.querySelector("div.col-md-9 > div > div > div > div > div.amcharts-main-div")
+      .parentElement;
+    Object.assign(chartNode.style, { height });
+  }
 }
-new biggerAmcharts();
+
+new BiggerAmcharts();
--- components/bondTracker.js
+++ components/bondTracker.js
@@ -1,7 +1,15 @@
 const BaseComponent = require("../tools/baseComponent.js");
 const { tools, componentList, runtimeData, indexDBData, feature_config, langData } = require("../tools/tools.js");
 
+/**
+ * @class bondTracker
+ * @augments BaseComponent
+ * @description 用于定时查询当前债券市场是否有符合要求的债券可供购买。
+ */
 class bondTracker extends BaseComponent {
+  /**
+   * @constructor
+   */
   constructor() {
     super();
     this.name = "债券市场追踪器";
@@ -9,6 +17,15 @@ class bondTracker extends BaseComponent {
     this.enable = true;
     this.tagList = ["债券", "追踪"];
   }
+
+  /**
+   * @property {object} indexDBData - 存储在 IndexedDB 中的数据。
+   * @property {Array<object>} indexDBData.realmData - 不同服务器（R1/R2）的追踪设置。
+   * @property {number} indexDBData.realmData[].minAmount - 最低数量。
+   * @property {number} indexDBData.realmData[].minCredit - 最低信用评级。
+   * @property {number} indexDBData.realmData[].minInterest - 最低利率。
+   * @property {boolean} indexDBData.autoTrack - 是否自动追踪。
+   */
   indexDBData = {
     realmData: [
       { // R1
@@ -23,15 +40,35 @@ class bondTracker extends BaseComponent {
     ],
     autoTrack: false, // 自动追踪
   }
+
+  /**
+   * @property {object} componentData - 组件内部数据。
+   * @property {string} componentData.baseURL - 债券市场 API 的基础 URL。
+   * @property {Array<string>} componentData.creditRatingList - 信用评级列表。
+   * @property {number|undefined} componentData.trackerFlag - 追踪器定时器的标识符。
+   */
   componentData = {
     baseURL: "https://www.simcompanies.com/api/bonds/rating/AAA-to-D/",
     creditRatingList: ["AAA", "AA+", "AA", "AA-", "A+", "A", "A-", "BBB+", "BBB", "BBB-", "BB+", "BB", "BB-", "B+", "B", "B-", "C", "D"],
     trackerFlag: undefined, // 追踪器标识
   }
+
   startupFuncList = [
     this.startTracker
   ]
+
+  /**
+   * @function frontUI
+   * @description 前端 UI 入口，触发追踪器处理函数。
+   * @returns {void}
+   */
   frontUI = () => this.trackerHandle("common");
+
+  /**
+   * @function settingUI
+   * @description 生成设置界面的 HTML 元素。
+   * @returns {Promise<HTMLElement>} 设置界面的主 DOM 节点。
+   */
   settingUI = async () => {
     let mainNode = document.createElement("div");
     let htmlText = `<div class=header>债券市场追踪器设置</div><div class=container><div><button class="btn script_opt_submit">保存更改</button></div><table><thead><tr><td>功能<td>设置<tbody><tr><td>当前服务器<td>######<tr><td title=勾选就打开定时追踪,会在1-2分钟钟随机一个间隔进行周期性检查>定时追踪<td><input class=form-control type=checkbox ######><tr><td title=如题>最低数量<td><input class=form-control type=number value=######><tr><td title=如题>最低评级<td><select class=form-control><option value=0>AAA<option value=1>AA+<option value=2>AA<option value=3>AA-<option value=4>A+<option value=5>A<option value=6>A-<option value=7>BBB+<option value=8>BBB<option value=9>BBB-<option value=10>BB+<option value=11>BB<option value=12>BB-<option value=13>B+<option value=14>B<option value=15>B-<option value=16>C<option value=17>D</select><tr><td title=如题>最低利息<td><input class=form-control type=number value=######></table></div>`;
@@ -46,11 +83,26 @@ class bondTracker extends BaseComponent {
     mainNode.addEventListener('click', event => this.settingClickHandle(event));
     return mainNode;
   }
+
+  /**
+   * @function settingClickHandle
+   * @description 处理设置界面点击事件。
+   * @param {Event} event - 点击事件对象。
+   * @returns {void}
+   */
   settingClickHandle(event) {
     if (event.target.className.match("script_opt_submit")) return this.settingSubmit();
   }
+
+  /**
+   * @function settingSubmit
+   * @description 提交设置表单并保存数据。
+   * @returns {Promise<void>}
+   */
   async settingSubmit() {
-    let valueList = Object.values(document.querySelectorAll("div#script_bondTracker_setting input, div#script_bondTracker_setting select"))
+    // 优化：将 DOM 查询移到函数外部或只查询一次
+    const settingInputs = document.querySelectorAll("div#script_bondTracker_setting input, div#script_bondTracker_setting select");
+    let valueList = Object.values(settingInputs)
       .map(node => node.type == "checkbox" ? node.checked : node.value);
     let realm = await tools.getRealm();
     // 初始化审查
@@ -70,11 +122,23 @@ class bondTracker extends BaseComponent {
     return tools.alert("保存并重启更新");
   }
 
+  /**
+   * @function startTracker
+   * @description 启动债券追踪器定时任务。
+   * @returns {void}
+   */
   startTracker() {
     if (this.componentData.trackerFlag) clearInterval(this.componentData.trackerFlag);
     if (!this.indexDBData.autoTrack) return;
     this.componentData.trackerFlag = setInterval(() => this.trackerHandle("slient"), tools.getRandomNumber(60 * 1000, 120 * 1000, parseInt));
   }
+
+  /**
+   * @function trackerHandle
+   * @description 处理债券追踪逻辑，获取并筛选债券数据。
+   * @param {string} [mode="common"] - 追踪模式，可以是 "common" 或 "slient"。
+   * @returns {Promise<void>}
+   */
   async trackerHandle(mode = "common") {
     let realm = await tools.getRealm();
     let netData = await tools.getNetData(this.componentData.baseURL);
@@ -87,6 +151,7 @@ class bondTracker extends BaseComponent {
       let interestCheck = bond.interest >= targetRef.minInterest;
       return creditCheck && amountCheck && interestCheck;
     }).map(bond => {
+      // 优化：使用模板字符串
       return `${bond.seller.company}发售了${bond.amount}只利率为${bond.interest}的股票,其信用评级为:${bond.seller.rating}`;
     });
     if (msgList.length == 0 && mode != "slient") return tools.msg_send("债券市场追踪器", "未找到符合要求的债券.");
